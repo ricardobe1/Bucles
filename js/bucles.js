ce500d1e2782518e0db4c09a539c2ce4da1f8a63
ricardobe1 <102460673+ricardobe1@users.noreply.github.com>
2022-03-31 00:41:14 -0600

    Add files via upload

---
+++ js/bucles.js
@@ -0,0 +1,17 @@
+for(var vuelta=0;vuelta <=500; vuelta++){
+    if(vuelta>=2){
+        document.write(" "+vuelta+"pulgadas");   
+    }else{
+        document.write(" "+vuelta+" pulgada");   
+    }
+    
+
+}
+document.write("<br>")
+var conteo=0;
+
+while(conteo<=700){
+    document.write(" "+conteo+" yardas ");
+   
+    conteo++;
+}
